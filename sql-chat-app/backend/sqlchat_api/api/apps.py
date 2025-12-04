c809052a76a71ac4a0fc2117c52bebb33d77d0aa
saadkhi <saadalioffic@gmail.com>
2025-12-04 21:41:22 +0500

    new changes

--- sql-chat-app/backend/sqlchat_api/api/apps.py
+++ sql-chat-app/backend/sqlchat_api/api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SqlPilotAppConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sql_pilot_app'
+    name = 'api'
--- sql-chat-app/backend/sqlchat_api/sqlchat_api/urls.py
+++ sql-chat-app/backend/sqlchat_api/sqlchat_api/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sql_pilot project.
+URL configuration for sqlchat_api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
